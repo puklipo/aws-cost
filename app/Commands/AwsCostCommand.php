a7c95b3f486fd27a679149a0e127020c355edba0
sds <sds.engineer.team@gmail.com>
2021-08-31 13:15:34 +0900

    date

--- app/Commands/AwsCostCommand.php
+++ app/Commands/AwsCostCommand.php
@@ -32,18 +32,18 @@ class AwsCostCommand extends Command
      */
     public function handle()
     {
-        $start = today()->startOfMonth();
-        $end = today();
+        $start = today()->startOfMonth()->toDateString();
+        $end = today()->toDateString();
 
-        if ($start->eq($end)) {
-            $start = today()->subMonthNoOverflow()->startOfMonth();
-            $end = today()->startOfMonth();
+        if ($start === $end) {
+            $start = today()->subMonthNoOverflow()->startOfMonth()->toDateString();
+            $end = today()->startOfMonth()->toDateString();
         }
 
         $result = Aws::createCostExplorer()->getCostAndUsage([
             'TimePeriod' => [
-                'Start' => $start->toDateString(),
-                'End' => $end->toDateString(),
+                'Start' => $start,
+                'End' => $end,
             ],
             'Granularity' => 'MONTHLY',
             'Metrics' => ['AmortizedCost'],
