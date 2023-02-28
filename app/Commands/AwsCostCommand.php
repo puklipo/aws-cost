06f2712a42a9320579afcde26c6928ab49e3a45e
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-02-28 10:08:28 +0900

    10

--- app/Commands/AwsCostCommand.php
+++ app/Commands/AwsCostCommand.php
@@ -3,7 +3,6 @@
 namespace App\Commands;
 
 use Aws\Sdk;
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Arr;
 use LaravelZero\Framework\Commands\Command;
 use Revolution\Line\Facades\LineNotify;
@@ -27,10 +26,8 @@ class AwsCostCommand extends Command
 
     /**
      * Execute the console command.
-     *
-     * @return int
      */
-    public function handle()
+    public function handle(): int
     {
         $start = today()->startOfMonth()->toDateString();
         $end = today()->toDateString();
@@ -54,22 +51,19 @@ class AwsCostCommand extends Command
         $total = Arr::get($result->toArray(), 'ResultsByTime.0.Total.AmortizedCost.Amount');
         $total = number_format($total, 2);
 
-        $message = "\n${start} ~ ${end}\n${total} USD";
+        $message = collect([
+            PHP_EOL,
+            $start,
+            ' ~ ',
+            $end,
+            PHP_EOL,
+            $total,
+            ' USD',
+        ])->join('');
 
         LineNotify::withToken(config('line.notify.personal_access_token'))
                   ->notify(LineNotifyMessage::create($message)->toArray());
 
         return 0;
-    }
-
-    /**
-     * Define the command's schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
     }
 }
